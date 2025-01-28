4b7d24a6862e0bb4d5508ae54c50260e1ab0100c
baptisteDULIEUX <baptiste.dulieux25420@gmail.com>
2025-01-28 08:46:47 +0100

    Initial Commit

---
+++ TodoB1/TodoB1App.swift
@@ -0,0 +1,17 @@
+//
+//  TodoB1App.swift
+//  TodoB1
+//
+//  Created by dulieux baptiste on 28/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoB1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
